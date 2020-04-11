0fde67ff6d5acb636d5df8a5fee9bbc75d849bcd
Nipun Mediratta <nipunmediratta@Nipuns-MacBook-Pro.local>
2020-04-10 20:58:15 -0700

    Modified metamap.py to add meta tags to the input text

--- MetaMap/metamap.py
+++ MetaMap/metamap.py
@@ -1,20 +1,23 @@
 from pymetamap import MetaMap
 
 def metamap(path,dataset):
+	semantic_types ={}
 	length = len(dataset)
 	indices = [i+1 for i in range(len(dataset))]
 	mm = MetaMap.get_instance(path)
 	concepts,error = mm.extract_concepts(dataset,indices)
 	if not error:
-		return concepts
+		for concept in concepts:
+			semantic_types[concept.preferred_name] = concept.semtypes
+		return semantic_types
 
 
 def main():
 	metamap_path = input("Please enter the path where your metamap binary file is located")
-	dataset = ['Heart Attack', 'John had a huge heart attack']
-	concepts = metamap(metamap_path,dataset)
-	for concept in concepts:
-		print(concept)
+	#dataset = ['Heart Attack', 'John had a huge heart attack']
+	dataset = ['I have Diabetes and I cough a lot', 'John had a huge heart attack']
+	semantic_types = metamap(metamap_path,dataset)
+	print(semantic_types)
 
 main()
 
