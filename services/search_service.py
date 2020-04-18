6e118ecb8276777527f36f67e1e93ee599c882b7
DivyaKshatriya <divyakshatriya12@gmail.com>
2020-04-18 15:22:23 -0700

    added ranking algorithm
    tested on the local system

--- services/search_service.py
+++ services/search_service.py
@@ -20,10 +20,56 @@ class SearchService():
     def rank_posts(self, posts):
         res = []
         # i = 0
-        for post in posts:
-            final_score = post["score"] + post["trustworthiness"] + post["freshness"] + post["hotness"] + post["length"]
+        result = self.normalize(posts)
+        for post in result:
+            final_score = post["score"] + post["trustworthiness"] + post["freshness"] + post["hotness"] + post["normalized-length"]
             post["final_score"] = final_score
             # i += 1
             res.append(post)
         res = sorted(res, key=lambda x:x['final_score'], reverse = True)
         return res
+
+    def normalize(self,posts):
+        time_created = []
+        no_of_replies =[]
+        lengths = []
+        author_weights = []
+        posts_ = []
+        tf_idf = []
+        for post in posts:
+            post=json.loads(post)
+            time_created.append(post["lastActivityTs"])
+            no_of_replies.append(post['numExpertReplies'])
+            lengths.append(post['numWords'])
+            author_weights.append(post['authorsWeight'])
+            posts_.append(post)
+            tf_idf.append(posts['score'])
+        
+        #freshness
+        maxTime = max(time_created)
+        sf = [val/maxTime for val in time_created]
+        
+        #hotness
+        maxReplies = max(no_of_replies)
+        sh = [val/maxReplies for val in no_of_replies]
+        
+        #length
+        max_smr = max(tf-idf)
+        max_length = max(lengths)
+        length_posts = [((lengths[i]/max_length)*max_smr)for i in range(len(lengths))]
+        
+        #trusthworthiness
+        st = [(author_weights[i]*max_smr) for i in range(len(author_weights))]
+        max_st = max(st)
+        st_normalized =[(val/max_st) for val in st]
+
+        i = 0
+
+        for post in posts_:
+            post['freshness'] = sf[i]
+            post['hotness'] = sh[i]
+            post['normalized-length'] = length_posts[i]
+            post['trusthworthiness'] = st_normalized[i]
+            i+=1
+            
+        return posts_
